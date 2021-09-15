9465e44bdf943d36850835913b0502aab098abdb
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-09-16 00:02:34 +0600

    button updated

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4500,
       disableOnInteraction: false,
     },
     pagination: {
@@ -53,7 +53,7 @@ var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 3500,
       disableOnInteraction: false,
     },
     loop: true,
